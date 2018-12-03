a73ae0bb6bbeb5bb288320d97491ccd3ebf80508
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2018-12-03 09:49:26 -0500

    Add toString() method
    Returns value of instance variable

--- GitTest.java
+++ GitTest.java
@@ -1,11 +1,17 @@
 public class GitTest{
 
 	private String statement;
-	
+
 	public GitTest(){
-	
+
 		statement = "This is my GitHub test";
-	
+
+	}
+
+	public String toString(){
+
+		return statement;
+
 	}
 
 }
